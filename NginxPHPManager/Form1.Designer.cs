1167cb692d8130c7092380b3e3f1a4fc2b7f8174
uanaoeng <39598014+uanaoeng@users.noreply.github.com>
2019-02-03 22:25:24 +0800

    版本1.0发布

---
+++ NginxPHPManager/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace NginxPHPManager
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNginxStart = new System.Windows.Forms.Button();
+            this.btnNginxStop = new System.Windows.Forms.Button();
+            this.btnNginxQuit = new System.Windows.Forms.Button();
+            this.btnNginxReload = new System.Windows.Forms.Button();
+            this.btnNginxReopen = new System.Windows.Forms.Button();
+            this.btnPHPStart = new System.Windows.Forms.Button();
+            this.btnPHPStop = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnNginxStart
+            // 
+            this.btnNginxStart.Location = new System.Drawing.Point(6, 61);
+            this.btnNginxStart.Name = "btnNginxStart";
+            this.btnNginxStart.Size = new System.Drawing.Size(100, 30);
+            this.btnNginxStart.TabIndex = 0;
+            this.btnNginxStart.Text = "运行";
+            this.btnNginxStart.UseVisualStyleBackColor = true;
+            this.btnNginxStart.Click += new System.EventHandler(this.btnNginxStart_Click);
+            // 
+            // btnNginxStop
+            // 
+            this.btnNginxStop.Location = new System.Drawing.Point(6, 97);
+            this.btnNginxStop.Name = "btnNginxStop";
+            this.btnNginxStop.Size = new System.Drawing.Size(100, 30);
+            this.btnNginxStop.TabIndex = 2;
+            this.btnNginxStop.Text = "柔性中止";
+            this.btnNginxStop.UseVisualStyleBackColor = true;
+            this.btnNginxStop.Click += new System.EventHandler(this.btnNginxStop_Click);
+            // 
+            // btnNginxQuit
+            // 
+            this.btnNginxQuit.Location = new System.Drawing.Point(112, 61);
+            this.btnNginxQuit.Name = "btnNginxQuit";
+            this.btnNginxQuit.Size = new System.Drawing.Size(100, 30);
+            this.btnNginxQuit.TabIndex = 3;
+            this.btnNginxQuit.Text = "强制中止";
+            this.btnNginxQuit.UseVisualStyleBackColor = true;
+            this.btnNginxQuit.Click += new System.EventHandler(this.btnNginxQuit_Click);
+            // 
+            // btnNginxReload
+            // 
+            this.btnNginxReload.Location = new System.Drawing.Point(112, 97);
+            this.btnNginxReload.Name = "btnNginxReload";
+            this.btnNginxReload.Size = new System.Drawing.Size(100, 30);
+            this.btnNginxReload.TabIndex = 4;
+            this.btnNginxReload.Text = "重载配置";
+            this.btnNginxReload.UseVisualStyleBackColor = true;
+            this.btnNginxReload.Click += new System.EventHandler(this.btnNginxReload_Click);
+            // 
+            // btnNginxReopen
+            // 
+            this.btnNginxReopen.Location = new System.Drawing.Point(6, 135);
+            this.btnNginxReopen.Name = "btnNginxReopen";
+            this.btnNginxReopen.Size = new System.Drawing.Size(100, 30);
+            this.btnNginxReopen.TabIndex = 5;
+            this.btnNginxReopen.Text = "重载日志";
+            this.btnNginxReopen.UseVisualStyleBackColor = true;
+            this.btnNginxReopen.Click += new System.EventHandler(this.btnNginxReopen_Click);
+            // 
+            // btnPHPStart
+            // 
+            this.btnPHPStart.Location = new System.Drawing.Point(6, 59);
+            this.btnPHPStart.Name = "btnPHPStart";
+            this.btnPHPStart.Size = new System.Drawing.Size(100, 30);
+            this.btnPHPStart.TabIndex = 7;
+            this.btnPHPStart.Text = "运行";
+            this.btnPHPStart.UseVisualStyleBackColor = true;
+            this.btnPHPStart.Click += new System.EventHandler(this.btnPHPStart_Click);
+            // 
+            // btnPHPStop
+            // 
+            this.btnPHPStop.Location = new System.Drawing.Point(112, 59);
+            this.btnPHPStop.Name = "btnPHPStop";
+            this.btnPHPStop.Size = new System.Drawing.Size(100, 30);
+            this.btnPHPStop.TabIndex = 9;
+            this.btnPHPStop.Text = "中止";
+            this.btnPHPStop.UseVisualStyleBackColor = true;
+            this.btnPHPStop.Click += new System.EventHandler(this.btnPHPStop_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnNginxStart);
+            this.groupBox1.Controls.Add(this.btnNginxStop);
+            this.groupBox1.Controls.Add(this.btnNginxQuit);
+            this.groupBox1.Controls.Add(this.btnNginxReload);
+            this.groupBox1.Controls.Add(this.btnNginxReopen);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(218, 171);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nginx";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "状态：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnPHPStart);
+            this.groupBox2.Controls.Add(this.btnPHPStop);
+            this.groupBox2.Location = new System.Drawing.Point(12, 189);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(218, 99);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "php-cgi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "状态：";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 299);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nginx & PHP 管理";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNginxStart;
+        private System.Windows.Forms.Button btnNginxStop;
+        private System.Windows.Forms.Button btnNginxQuit;
+        private System.Windows.Forms.Button btnNginxReload;
+        private System.Windows.Forms.Button btnNginxReopen;
+        private System.Windows.Forms.Button btnPHPStart;
+        private System.Windows.Forms.Button btnPHPStop;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ NginxPHPManager/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Xml;
+using System.IO;
+
+namespace NginxPHPManager
+{
+    public partial class mainForm : Form
+    {
+
+// =============================================================================
+//
+// 全局变量声明
+//
+// =============================================================================
+
+        static string nginxPath;
+        static string phpPath;
+        static string arguForPHPcgi;
+
+// =============================================================================
+//
+// 基本方法
+//
+// =============================================================================
+
+        // 构造方法
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        // 从当前程序所在目录下的配置文件 npm.xml 中读取配置文件。
+        static void readDataFromJXMLFile()
+        {
+
+            XmlDocument doc = new XmlDocument();
+            string myConfigs = System.IO.File.ReadAllText(@"npm.xml");
+            doc.LoadXml(myConfigs);
+            XmlNode root = doc.FirstChild;
+
+            phpPath = root.SelectSingleNode("phpPath").InnerText;
+            nginxPath = root.SelectSingleNode("nginxPath").InnerText;
+            arguForPHPcgi = root.SelectSingleNode("arguForPHPcgi").InnerText;
+
+        }
+
+        // 判断指定的进程是否已经存在
+        static bool checkIfProcessRunning(string processName)
+        {
+            Process[] process = Process.GetProcessesByName(processName);
+            if (process.Length != 0)
+                return true;
+            else
+                return false;
+        }
+
+// =============================================================================
+//
+// 事件响应
+//
+// =============================================================================
+
+        // 窗体加载时触发
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            readDataFromJXMLFile();
+
+            if (checkIfProcessRunning("nginx"))
+            {
+                this.label1.Text = "状态：运行中";
+            }
+            else
+            {
+                this.label1.Text = "状态：未运行";
+                this.btnNginxQuit.Enabled = false;
+                this.btnNginxStop.Enabled = false;
+                this.btnNginxReload.Enabled = false;
+                this.btnNginxReopen.Enabled = false;
+            }
+
+            if (checkIfProcessRunning("php-cgi"))
+            {
+                this.label2.Text = "状态：运行中";
+            }
+            else
+            {
+                this.label2.Text = "状态：未运行";
+                this.btnPHPStop.Enabled = false;
+            }
+        }
+
+// -----------------------------------------------------------------------------
+
+        private void btnNginxStart_Click(object sender, EventArgs e)
+        {
+            if(checkIfProcessRunning("nginx"))
+            {
+                MessageBox.Show("nginx 已经在运行了，请勿重复启用。");
+            }
+            else
+            {
+                Process processName = new Process();
+                processName.StartInfo.FileName = nginxPath + @"\nginx.exe";
+                processName.StartInfo.WorkingDirectory = nginxPath;
+                processName.StartInfo.UseShellExecute = false;
+                processName.StartInfo.CreateNoWindow = true;
+                processName.Start();
+                this.label1.Text = "状态：运行中";
+                this.btnNginxQuit.Enabled = true;
+                this.btnNginxStop.Enabled = true;
+                this.btnNginxReload.Enabled = true;
+                this.btnNginxReopen.Enabled = true;
+            }
+        }
+
+// -----------------------------------------------------------------------------
+
+        private void btnNginxStop_Click(object sender, EventArgs e)
+        {
+            if(checkIfProcessRunning("nginx"))
+            {
+                Process processName = new Process();
+                processName.StartInfo.FileName = nginxPath + @"\nginx.exe";
+                processName.StartInfo.WorkingDirectory = nginxPath;
+                processName.StartInfo.Arguments = "-s stop";
+                processName.StartInfo.UseShellExecute = false;
+                processName.StartInfo.CreateNoWindow = true;
+                processName.Start();
+                this.label1.Text = "状态：未运行";
+                this.btnNginxQuit.Enabled = false;
+                this.btnNginxStop.Enabled = false;
+                this.btnNginxReload.Enabled = false;
+                this.btnNginxReopen.Enabled = false;
+            }
+        }
+
+// -----------------------------------------------------------------------------
+
+        private void btnNginxQuit_Click(object sender, EventArgs e)
+        {
+            if (checkIfProcessRunning("nginx"))
+            {
+                Process processName = new Process();
+                processName.StartInfo.FileName = nginxPath + @"\nginx.exe";
+                processName.StartInfo.WorkingDirectory = nginxPath;
+                processName.StartInfo.Arguments = "-s quit";
+                processName.StartInfo.UseShellExecute = false;
+                processName.StartInfo.CreateNoWindow = true;
+                processName.Start();
+                this.label1.Text = "状态：未运行";
+                this.btnNginxQuit.Enabled = false;
+                this.btnNginxStop.Enabled = false;
+                this.btnNginxReload.Enabled = false;
+                this.btnNginxReopen.Enabled = false;
+            }
+        }
+
+// -----------------------------------------------------------------------------
+
+        private void btnNginxReload_Click(object sender, EventArgs e)
+        {
+            if (checkIfProcessRunning("nginx"))
+            {
+                Process processName = new Process();
+                processName.StartInfo.FileName = nginxPath + @"\nginx.exe";
+                processName.StartInfo.WorkingDirectory = nginxPath;
+                processName.StartInfo.Arguments = "-s reload";
+                processName.StartInfo.UseShellExecute = false;
+                processName.StartInfo.CreateNoWindow = true;
+                processName.Start();
+                MessageBox.Show("重新载入配置文件，成功。");
+            }
+        }
+
+// -----------------------------------------------------------------------------
+
+        private void btnNginxReopen_Click(object sender, EventArgs e)
+        {
+            if (checkIfProcessRunning("nginx"))
+            {
+                Process processName = new Process();
+                processName.StartInfo.FileName = nginxPath + @"\nginx.exe";
+                processName.StartInfo.WorkingDirectory = nginxPath;
+                processName.StartInfo.Arguments = "-s reopen";
+                processName.StartInfo.UseShellExecute = false;
+                processName.StartInfo.CreateNoWindow = true;
+                processName.Start();
+                MessageBox.Show("重新打开日志文件，成功。");
+            }
+        }
+
+// -----------------------------------------------------------------------------
+
+        private void btnPHPStart_Click(object sender, EventArgs e)
+        {
+            if (checkIfProcessRunning("php-cgi"))
+            {
+                MessageBox.Show("php-cgi 已经在运行了，请勿重复启用。");
+            }
+            else
+            {
+                Process processName = new Process();
+                processName.StartInfo.FileName = phpPath + @"\php-cgi.exe";
+                processName.StartInfo.Arguments = arguForPHPcgi;
+                processName.StartInfo.UseShellExecute = false;
+                processName.StartInfo.CreateNoWindow = true;
+                processName.Start();
+                this.label2.Text = "状态：运行中";
+                this.btnPHPStop.Enabled = true;
+            }
+        }
+
+// -----------------------------------------------------------------------------
+
+        private void btnPHPStop_Click(object sender, EventArgs e)
+        {
+            if (checkIfProcessRunning("php-cgi"))
+            {
+                Process processName = new Process();
+                processName.StartInfo.FileName = "taskkill.exe";
+                processName.StartInfo.Arguments = "/im php-cgi.exe /f";
+                processName.StartInfo.UseShellExecute = false;
+                processName.StartInfo.CreateNoWindow = true;
+
+                foreach (Process p in Process.GetProcessesByName("php-cgi"))
+                {
+                    processName.Start();
+                    this.label2.Text = "状态：未运行";
+                    this.btnPHPStop.Enabled = false;
+                }
+            }
+
+        }
+
+    }
+}
